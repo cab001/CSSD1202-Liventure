7245a5a67243f54c47f11902f37b56f1f736bb9e
Katie-Lui <katielui51@gmail.com>
2026-03-28 02:08:02 -0400

    worked on js of habits html (the part that lets the user toggle what stats to track)
    -created a "hide" class in css
    -gave ids to some divs and elements
    - made the checkboxes of the default stats to track checked by default
    - made the "Oz of water:" label and its input box hide when "Water" is unchecked

--- habits.js
+++ habits.js
@@ -7,11 +7,35 @@ let exerciseToggle = document.getElementById("exerciseToggle");
     // implement custom stat if theres time, maybe even a plus button to add more of them (and select them using nextsibling)
 
 let toggleForm = document.getElementById("toggleForm");
-toggleForm.addEventListener("submit", checkToggles);
+//toggleForm.addEventListener("submit", checkToggles);
 // instead of using form submit, to better comply wuth assignment req, could use click event on each toggle instead and then toggling a class that hides stuff
+waterToggle.addEventListener("click", checkToggles);
+sleepToggle.addEventListener("click", checkToggles);
+mealsToggle.addEventListener("click", checkToggles);
+exerciseToggle.addEventListener("click", checkToggles);
+
+let dailyWater = document.getElementById("dailyWater");
+let dailySleep = document.getElementById("sleep");
+let dailyMeals = document.getElementById("meals");
+let dailyExercise = document.getElementById("exercise");
+
+let waterGoalDiv = document.getElementById("waterGoalDiv");
+let sleepGoal = document.getElementById("sleepGoal");
+let mealsGoal = document.getElementById("mealsGoal");
+let exerciseGoal = document.getElementById("exerciseGoal");
+
+let waterStreakLabel = document.getElementById("waterStreakLabel")
+// make all streak labels vars, then add them to have their hide classes added&removed in checkToggles func
+
 function checkToggles() {
     if (waterToggle.checked) {
         //remove any hide class or attribute from the stuff that show water goal and progress (ex. Oz of water under tdy's stats and water: under goals:)
+        dailyWater.classList.remove("hide");
+        waterGoalDiv.classList.remove("hide");
+    }
+    else {
+        dailyWater.classList.add("hide");
     }
     // do if statement for all others 
 }
+// also make it in html code(?) that stats to track's checkboxes r auto checked at the beginning
