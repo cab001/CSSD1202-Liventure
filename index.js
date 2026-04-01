74d639463c9ff0c2cd2b855500af57da6c7c4513
Katie-Lui <katielui51@gmail.com>
2026-04-01 02:33:04 -0400

    added 2 avatar options
    - added chloe's 2 avatar images
    - removed default one from before
    - removed habit streak text
    - added buttons that allow for the image to change based on user choice

--- index.js
+++ index.js
@@ -21,3 +21,22 @@ habitStreakP.textContent = "Habit Streak: " + 0;
 // if have time, can make customizable avatar, and when hover over a hair option of the user's choice, replace the current choice with the one being hovered
 // ex. if they have short hair equipped but their mouse is hovering over the long hair, show the long hair and hide the short hair
 // otherwise just add a picture for a default avatar in the html
+
+let longHair = document.getElementById("longHair");
+let shortHair = document.getElementById("shortHair");
+
+let longHairBtn = document.getElementById("longHairBtn");
+let shortHairBtn = document.getElementById("shortHairBtn");
+
+longHairBtn.addEventListener("click", longHairClicked);
+shortHairBtn.addEventListener("click", shortHairClicked);
+
+function longHairClicked() {
+    shortHair.classList.add("hide");
+    longHair.classList.remove("hide");
+}
+
+function shortHairClicked() {
+    longHair.classList.add("hide");
+    shortHair.classList.remove("hide");
+}
